7b3b9a79919ca7fadbdaf866d977a3397fdb3f05
Rajan <acharya.rajan.raj11@gmail.com>
2025-02-23 10:24:11 +0545

    added: utils code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import express from "express";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-connectDB();
+const port = process.env.PORT || 8080;
+
+connectDB()
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log("Database Connection failed!! ERR:", err);
+    });
