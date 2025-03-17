070befb4405eaa4c4bdb8098620c844da5ee9214
Rajan <acharya.rajan.raj11@gmail.com>
2025-03-17 22:54:07 +0545

    feat ✨: add comment routes for managing video comments

--- src/app.js
+++ src/app.js
@@ -20,8 +20,10 @@ app.use(cookieParser());
 
 //routes import
 import userRouter from "./routes/user.routes.js";
+import commentRouter from "./routes/comment.routes.js";
 
 //routes declaration
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/comments", commentRouter);
 
 export { app };
---
+++ src/routes/comment.routes.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import {
+  addComment,
+  getVideoComments,
+} from "../controllers/comment.controllers.js";
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
+
+const router = Router();
+router.route("/video-comments/:videoId").get(verifyJWT, getVideoComments);
+router.route("/add-comment/:videoId").post(verifyJWT, addComment);
+router.route("/update-comment/:commentId").patch(verifyJWT, updateComment);
+router.route("/delete-comment/:commentId").delete(verifyJWT, deleteComment);
+
+export default router;
