c138a40e90fc3e09412e789503a83d9ef3a0b824
Rajan Raj Acharya <acharya.rajan.raj11@gmail.com>
2025-04-22 22:31:29 +0545

    fix :bug: : correct import statements for delete and update tweet functions in tweet routes

--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import {
   createTweet,
+  deleteTweet,
   getUserTweets,
+  updateTweet,
 } from "../controllers/tweet.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
