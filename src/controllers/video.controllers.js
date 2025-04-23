dfdfccf70df21227b9b4a91283551bd9f437ffee
Rajan Raj Acharya <acharya.rajan.raj11@gmail.com>
2025-04-23 09:39:29 +0545

    fix :bug: : improve error handling and code clarity in publishAVideo function

--- src/controllers/video.controllers.js
+++ src/controllers/video.controllers.js
@@ -51,6 +51,7 @@ const publishAVideo = asyncHandler(async (req, res) => {
     throw new ApiError(400, "Title and description are required");
   }
 
+  //getting the local path of the video and thumbnail
   const videoLocalPath = req.files?.video[0]?.path;
 
   const thumbnailLocalPath = req.files?.thumbnail[0]?.path;
@@ -58,21 +59,34 @@ const publishAVideo = asyncHandler(async (req, res) => {
   if (!videoLocalPath) {
     throw new ApiError(400, "Video file is required");
   }
-  const thumbnailUploaded = await uploadOnCloudinary(thumbnailLocalPath);
-  console.log(th);
+  if (!thumbnailLocalPath) {
+    throw new ApiError(400, "Thumbnail file is required");
+  }
 
   //uploading video on cloudinary
   const videoUploaded = await uploadOnCloudinary(videoLocalPath);
-  console.log(videoUploaded);
-  //   if (!videoUploaded.url) {
-  //     throw new ApiError(500, "Error uploading video to cloudinary");
-  //   }
+  if (!videoUploaded.url) {
+    throw new ApiError(500, "Error uploading video to cloudinary");
+  }
 
   //uploading thumbnail on cloudinary
-  //   if (!thumbnailUploaded.url) {
-  //     throw new ApiError(500, "Error uploading thumbnail to cloudinary");
-  //   }
+  const thumbnailUploaded = await uploadOnCloudinary(thumbnailLocalPath);
 
-  //   const video = await Video.create({});
+  if (!thumbnailUploaded.url) {
+    throw new ApiError(500, "Error uploading thumbnail to cloudinary");
+  }
+
+  const video = await Video.create({
+    description,
+    title,
+    videoFile: videoUploaded.secure_url,
+    thumbnail: thumbnailUploaded.secure_url,
+    duration: videoUploaded.duration,
+    owner: req.user._id,
+  });
+
+  res
+    .status(201)
+    .json(new ApiResponse(200, video, "Video published successfully"));
 });
 export { getAllVideos, publishAVideo };
