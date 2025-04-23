f81397549515fdc879343f5fb6c55622b6e17a9b
Rajan Raj Acharya <acharya.rajan.raj11@gmail.com>
2025-04-23 20:14:43 +0545

    feat ✨: reorganize video routes for improved structure and clarity

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,9 +1,18 @@
 import { Router } from "express";
-import { publishAVideo } from "../controllers/video.controllers.js";
+import {
+  deleteVideo,
+  getAllVideos,
+  getVideoById,
+  publishAVideo,
+  togglePublishStatus,
+  updateThumbnail,
+  updateVideo,
+} from "../controllers/video.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 
 const router = Router();
+router.route("/get-all-videos").get(getAllVideos);
 router.route("/publish-video").post(
   verifyJWT,
   upload.fields([
@@ -18,5 +27,14 @@ router.route("/publish-video").post(
   ]),
   publishAVideo
 );
+router.route("/get-video-by-id/:videoId").get(getVideoById);
+router.route("/update-video/:videoId").patch(verifyJWT, updateVideo);
+router
+  .route("/update-thumbnail/:videoId")
+  .patch(verifyJWT, upload.single("thumbnail"), updateThumbnail);
+router.route("/delete-video/:videoId").delete(verifyJWT, deleteVideo);
+router
+  .route("/toggle-publish-status/:videoId")
+  .patch(verifyJWT, togglePublishStatus);
 
 export default router;
