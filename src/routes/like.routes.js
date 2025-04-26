a8a67419cc3660d73adf1f61c3efbc7873b59042
Rajan Raj Acharya <acharya.rajan.raj11@gmail.com>
2025-04-26 18:03:46 +0545

    fix :bug: : add JWT verification to like routes for enhanced security

--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -5,11 +5,14 @@ import {
   toggleTweetLike,
   toggleVideoLike,
 } from "../controllers/like.controllers.js";
+import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
 const router = Router();
-router.route("/toggle-video-like/:videoId").post(toggleVideoLike);
-router.route("/toggle-comment-like/:commentId").post(toggleCommentLike);
-router.route("/toggle-tweet-like/:tweetId").post(toggleTweetLike);
-router.route("/get-liked-videos").get(getLikedVideos);
+router.route("/toggle-video-like/:videoId").post(verifyJWT, toggleVideoLike);
+router
+  .route("/toggle-comment-like/:commentId")
+  .post(verifyJWT, toggleCommentLike);
+router.route("/toggle-tweet-like/:tweetId").post(verifyJWT, toggleTweetLike);
+router.route("/get-liked-videos").get(verifyJWT, getLikedVideos);
 
 export default router;
