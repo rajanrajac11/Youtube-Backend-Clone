2ddf7eb23a0924f4b1015bf1fb8f52d69b15b33d
Rajan Raj Acharya <acharya.rajan.raj11@gmail.com>
2025-04-26 18:45:11 +0545

    fix :bug: : correct route definitions for update-playlist and delete-playlist to include leading slashes

--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -21,7 +21,7 @@ router
   .route("/remove-video-from-playlist/:playlistId/:videoId")
   .delete(verifyJWT, removeVideoFromPlaylist);
 
-router.route(" update-playlist/:playlistId").patch(verifyJWT, updatePlaylist);
-router.route("delete-playlist/:playlistId").delete(verifyJWT, deletePlaylist);
+router.route("/update-playlist/:playlistId").patch(verifyJWT, updatePlaylist);
+router.route("/delete-playlist/:playlistId").delete(verifyJWT, deletePlaylist);
 
 export default router;
